57572740bc2b23b010a4dca41e91db17428c4239
eeeyooon <102462534+eeeyooon@users.noreply.github.com>
2023-02-26 13:03:43 +0900

    [style] 총 상품 금액 관련 정보 튤팁 추가

--- src/components/PurchageInfo.js
+++ src/components/PurchageInfo.js
@@ -22,6 +22,28 @@ const PurchageInfo = ({ count }) => {
               <rect x="0.5" y="0.5" width="15" height="15" stroke="#DEE0E3" />
             </svg>
           </span>
+          <div className="total-price-info-tooltip">
+            <span>
+              총 상품금액에
+              <span className="color-blue">
+                배송비는 포함되어 있지 않습니다.{' '}
+              </span>
+              <br />
+              결제시 배송비가 추가될 수 있습니다.
+            </span>
+            <svg
+              width="12"
+              height="12"
+              viewBox="0 0 12 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M5.99999 6.70706L1.40379 11.3033L0.696686 10.5962L5.29288 5.99996L0.696688 1.40376L1.40379 0.696655L5.99999 5.29285L10.5962 0.696655L11.3033 1.40376L6.7071 5.99996L11.3033 10.5962L10.5962 11.3033L5.99999 6.70706Z"
+                fill="#333333"
+              />
+            </svg>
+          </div>
         </div>
         <div className="total-count-price">
           <div className="total-count">총 수량 {count}개</div>
