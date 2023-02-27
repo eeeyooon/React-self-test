9e326ea7fd39fe7b6193487dd67c6a04358b2a3f
eeeyooon <102462534+eeeyooon@users.noreply.github.com>
2023-02-27 20:07:53 +0900

    [refactor] 불필요한 훅 사용 정리

--- src/components/PurchageBtn.js
+++ src/components/PurchageBtn.js
@@ -1,47 +1,21 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const PurchageBtn = ({ count }) => {
   const [disable, setDisable] = useState(false);
 
-  const btn = useRef();
-  const logo = useRef();
-
-  const btnHover = () => {
-    if (!disable) {
-      btn.current.style.background = '#0AAE4F';
-    }
-  };
-
-  const btnOut = () => {
-    if (!disable) {
-      btn.current.style.background = '#03C75A';
-    }
-  };
-
   useEffect(() => {
     if (count == 0) {
-      btn.current.style.background = '#DEE0E3';
-      btn.current.style.color = '#999999';
-      logo.current.style.fill = '#999999';
-      btn.current.style.cursor = 'auto';
       setDisable(true);
     } else {
-      btn.current.style.background = '#03C75A';
-      btn.current.style.color = '#ffffff';
-      logo.current.style.fill = '#03C75A';
-      btn.current.style.cursor = 'pointer';
       setDisable(false);
     }
   }, [count]);
 
   return (
     <button
-      ref={btn}
-      onMouseOver={btnHover}
-      onMouseOut={btnOut}
       role="submit"
       aria-label="구매하기"
-      className="purchage-button"
+      className={`purchage-button ${disable ? 'disable' : 'not-disable'}`}
     >
       <svg
         width="28"
@@ -52,9 +26,8 @@ const PurchageBtn = ({ count }) => {
       >
         <rect width="28" height="28" fill="white" />
         <path
-          ref={logo}
           d="M7 7H11.719L16.2628 13.5971V7H21V21H16.2628L11.293 14.1082V21H7V7Z"
-          fill="#03C75A"
+          fill={`${disable ? '#999999' : '#03C75A'}`}
         />
       </svg>
 
